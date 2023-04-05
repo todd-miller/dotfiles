c4bf5c80da9845a047b89a39662d60932efa26ef
Todd Miller <todd_miller@skiplist.com>
2023-04-05 09:12:03 -0400

    update: trying to map visual block movements in lvim

--- lvim/.config/lvim/lua/user/keymaps.lua
+++ lvim/.config/lvim/lua/user/keymaps.lua
@@ -6,8 +6,18 @@ lvim.keys.insert_mode["jk"] = "<ESC>"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["<C-d>"] = "<C-d>zz";
-lvim.keys.normal_mode["<C-b>"] = "<C-b>zz";
-lvim.keys.normal_mode["n"] = "nzz";
-lvim.keys.normal_mode["N"] = "Nzz";
+lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
+lvim.keys.normal_mode["<C-b>"] = "<C-b>zz"
+lvim.keys.normal_mode["∆"] = ":m .+1<CR>=="
+lvim.keys.normal_mode["˚"] = ":m .-2<CR>=="
+lvim.keys.normal_mode["n"] = "nzz"
+lvim.keys.normal_mode["N"] = "Nzz"
+
+-- TODO - visual block movement mappings not working
+-- k == ˚
+-- lvim.keys.visual_mode["˚"] = ":m-2<CR>gv=gv"
+
+-- j == ∆
+-- lvim.keys.visual_mode["∆"] = ":m>+1<CR>gv=gv"
+
 
--- vim/.vimrc
+++ vim/.vimrc
@@ -1,5 +1,17 @@
 set hidden
 syntax on
+
+" Insert Mode 
+inoremap ∆ <Esc>:m .+1<CR>==gi
+inoremap ˚ <Esc>:m .-2<CR>==gi
+
+
+" Normal Mode 
 noremap jk <esc>
+nnoremap ∆ :m .+1<CR>==
+nnoremap ˚ :m .-2<CR>==
+
+" Visual Mode
 vnoremap jk <esc> 
+
 set timeoutlen=100
