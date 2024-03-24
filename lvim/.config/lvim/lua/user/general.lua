5993817993a4a98ec00367dcd6550b38a545d269
Todd Miller <MillerTodd@contractor.bfusa.com>
2024-03-24 15:35:18 -0400

    added code folding settings in general

--- lvim/.config/lvim/lua/user/general.lua
+++ lvim/.config/lvim/lua/user/general.lua
@@ -12,4 +12,10 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
+lvim.builtin.which_key.setup.plugins.presets.z = true
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.fillchars = "fold: "
+vim.wo.foldnestmax = 3
+vim.wo.foldminlines = 1
+vim.wo.foldlevel = 1
